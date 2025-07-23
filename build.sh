83fd6a10a60b85ecd1c134b49d62fd5576c59401
Patrick Thibaudeau <thibaud@yorku.ca>
2025-07-22 23:53:43 -0400

    Updated build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 docker buildx build --push \
 --platform linux/amd64,linux/arm64 \
---tag uitadmin/criabot:latest \
---tag uitadmin/criabot:v1.8.1 .
+--tag uitadmin/criabot:latest-beta \
+--tag uitadmin/criabot:v1.8.2-beta .
